a6fc2a94b6e878917b0c2821b2948bac17ff6dc3
Rita <rita.barragan@usp.ceu.es>
2025-02-18 18:00:05 +0100

    Subiendo proyecto a GitHub

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package aplicacion;
+
+import mates.Matematicas;
+
+public class Principal{
+    public static void main(String[] args){
+        System.out.println("El número PI es " +
+                Matematicas.generarNumeroPiIterativo(Integer.parseInt(args[0])));
+    }
+}
---
+++ src/mates/Matematicas.java
@@ -0,0 +1,12 @@
+package mates;
+
+public class Matematicas{
+    /**
+     * * Genera una aproximación al número Pi mediante el método de
+     * * Montecarlo. El parámetro `pasos` indica el número de puntos
+     * * generado.
+     * */
+    public static double generarNumeroPiIterativo(long pasos){
+        return 0; // Este código hay que cambiarlo.
+    }
+}
